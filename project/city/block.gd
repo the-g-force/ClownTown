5e80c17991c90a4e611190980e37ea674c1baa63
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-12-30 13:36:16 -0500

    Make unending road

---
+++ project/city/block.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+
+func _on_visible_on_screen_notifier_3d_screen_entered():
+	_add_next_block()
+
+
+func _add_next_block() -> void:
+	var next := duplicate()
+	next.position.z -= 5
+	get_parent().add_child(next)
