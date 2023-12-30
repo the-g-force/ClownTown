78509ac7b808a7ea92a6fc7fb1c20ccc7ce874bd
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-12-30 13:57:29 -0500

    Remove road blocks off-camera

--- project/city/block.gd
+++ project/city/block.gd
@@ -9,3 +9,7 @@ func _add_next_block() -> void:
 	var next := duplicate()
 	next.position.z -= 5
 	get_parent().add_child(next)
+
+
+func _on_visible_on_screen_notifier_3d_screen_exited():
+	queue_free()
